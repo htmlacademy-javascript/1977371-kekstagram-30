5a4cb067ec9bda204b90e4212ce2221c94e901de
EvgeniyDobrov <zhenek_kalinin@list.ru>
2023-10-15 13:49:11 +0600

    fix error

--- js/functions.js
+++ js/functions.js
@@ -3,10 +3,14 @@ function checkingString(string, length) {
   return string.length <= length;
 }
 
+// eslint-disable-next-line no-console
+console.log(checkingString);
 
 // Проверка строки на палиндром
 function palindrom (str) {
   str = str.toLowerCase().replaceAll(' ','');
-  return str == str.split('').reverse().join('');
+  return str === str.split('').reverse().join('');
 }
 
+// eslint-disable-next-line no-console
+console.log(palindrom);
