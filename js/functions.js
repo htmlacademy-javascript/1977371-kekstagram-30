c7d019e7c3c9a317d95e5c9f97fc6bcb60fba890
EvgeniyDobrov <zhenek_kalinin@list.ru>
2023-10-17 19:49:09 +0600

    добавил новую функцию

--- js/functions.js
+++ js/functions.js
@@ -13,3 +13,14 @@ function palindrom (str) {
 
 // eslint-disable-next-line no-console
 console.log(palindrom);
+
+// функция получения целого числа из заданного диапазона.
+function getRandomInteger (min, max) {
+  const lower = Math.ceil(Math.min(Math.abs(min), Math.abs(max)));
+  const upper = Math.floor(Math.max(Math.abs(min), Math.abs(max)));
+  const result = Math.random() * (upper - lower + 1) + lower;
+
+  return Math.floor(result);
+}
+// eslint-disable-next-line no-console
+console.log(getRandomInteger ());
