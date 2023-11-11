4f75fede8bff86afb23cbc5f4e6cc5e267a801c2
Keks <keks@htmlacademy.ru>
2023-11-11 15:26:29 +0300

    :heavy_check_mark: Сборка #8

---
+++ 8/js/main.js
@@ -0,0 +1,7 @@
+import {getPictures} from './data.js';
+import {renderThumbnails} from './thumbnail.js';
+
+renderThumbnails(getPictures());
+
+// eslint-disable-next-line no-console
+// console.log(getPictures());
