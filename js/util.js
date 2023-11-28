312a47b361212f6d14e646e91e96d76cf688c804
EvgeniyDobrov <zhenek_kalinin@list.ru>
2023-11-28 20:25:07 +0600

    подготовка к защите ч.2

--- js/util.js
+++ js/util.js
@@ -25,13 +25,4 @@ const debounce = (callback, timeoutDelay = PAUSE_IMG_RENDERING) => {
   };
 };
 
-// const getRandomInteger = (min, max) => {
-//   const lower = Math.ceil(Math.min(Math.abs(min), Math.abs(max)));
-//   const upper = Math.floor(Math.max(Math.abs(min), Math.abs(max)));
-//   const result = Math.random() * (upper - lower + 1) + lower;
-
-//   return Math.floor(result);
-// };
-
-
 export {showErrorMessage, isEscapeKey, debounce};
